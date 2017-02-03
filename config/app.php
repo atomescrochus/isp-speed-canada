c0f3bdaf781ec9b5b004ea5155be4a00141f2cba
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2017-02-03 10:32:12 -0500

    bulma starter kit

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Atomescrochus\LaravelBulmaStarter\LaravelBulmaStarterServiceProvider::class,
 
         /*
          * Application Service Providers...
